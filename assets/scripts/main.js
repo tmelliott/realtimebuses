89302acd3d525fb1c9dee6bec2b0d4511fca03be
Tom Elliott <tomelliottnz@gmail.com>
2017-07-18 15:51:50 +1200

    Plot the locations

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -21,47 +21,71 @@ function networkMap () {
         maxZoom: 19
     }).addTo(map);
 
-    var data = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                  "type": "Point",
-                  "coordinates": [174.7633, -36.8485]
-                },
-                "properties": {
-                  "delay": 30,
-                  "delaytype": "departure"
-                }
-            }
-        ]
+
+    var pts;
+
+    function loadData () {
+      protobuf.load("assets/protobuf/gtfs-realtime.proto", function(err, root) {
+          if (err)
+              throw err;
+          var f = root.lookupType("transit_realtime.FeedMessage");
+
+          var xhr = new XMLHttpRequest();
+          xhr.open("GET", "data/vehicle_locations.pb", true);
+          xhr.responseType = "arraybuffer";
+          xhr.onload = function(evt) {
+              var m = f.decode (new Uint8Array(xhr.response));
+              addPositions(m.entity);
+          }
+          xhr.send(null);
+      });
+    };
+    function addPositions (feed) {
+      console.log(feed);
+      var data = {
+          "type": "FeatureCollection",
+          "features": []
+      };
+      for (var i=0; i<feed.length; i++) {
+        if (feed[i].vehicle) {
+          if (feed[i].vehicle.position) {
+            data.features.push({
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [
+                  feed[i].vehicle.position.longitude,
+                  feed[i].vehicle.position.latitude
+                ]
+              },
+              "properties": {
+                "delay": 0,
+                "delaytype": "arrival"
+              }
+            });
+          }
+        }
+        console.log(feed[i]);
+      }
+      console.log(data);
+      if (pts != undefined) pts.clearLayers();
+      pts = L.geoJSON(data, {
+          pointToLayer: function(feature, latlng) {
+              return L.circleMarker(latlng, {
+                  radius: 2,
+                  fillColor: "#990000",
+                  weight: 0,
+                  fillOpacity: 0.8
+              });
+          }
+      }).addTo(map);
     };
 
-    var pts = L.geoJSON(data, {
-        pointToLayer: function(feature, latlng) {
-            return L.circleMarker(latlng, {
-                radius: 2,
-                fillColor: "#990000",
-                weight: 0,
-                fillOpacity: 0.8
-            });
-        }
-    }).addTo(map);
+    loadData();
     // setTimeout(function () {
     //     pts.clearLayers();
     //
     // }, 1000);
-
-    // var svg = d3.select(map.getPanes().overlayPane).append("svg"),
-    //     g = svg.append("g").attr("class", "leaflet-zoom-hide");
-    //
-    // d3.json("data/vp.json", function(error, collection) {
-    //     if (error) throw error;
-    //
-    //     var transform = d3.geo.transform({point: projectPoint}),
-    //       path = d3.geo.path().projection(transform);
-    // });
 };
 
 function networkStatus () {
