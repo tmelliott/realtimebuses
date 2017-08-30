002a3f0c64a784a5b5d49c0ad59a81f38241f5c6
Jono Cooper <jono@jonocooper.com>
2017-08-30 18:47:32 +1200

    Make Map Dark

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -19,7 +19,7 @@ function networkMap () {
         zoom: zoom,
         zoomControl: false
     });
-    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
         subdomains: 'abcd',
         maxZoom: 19
